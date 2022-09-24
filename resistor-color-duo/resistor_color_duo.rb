156700f948e611903bfe548771cc9e09ea792a17
Ahmed Shalaby <ashalaby@tahmeellogistics.com>
2022-09-24 06:08:23 +0200

    high scores

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -5,3 +5,26 @@ Write your code for the 'Resistor Color Duo' exercise in this file. Make the tes
 To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
+
+class ResistorColorDuo
+@colors = {
+  "black" => 0,
+  "brown" => 1,
+  "red" => 2,
+  "orange" => 3,
+  "yellow" => 4,
+  "green" => 5,
+  "blue" => 6,
+  "violet" => 7,
+  "grey" => 8,
+  "white" => 9
+}
+
+  def self.value(arr)
+
+    arr[0,2].map{ |x| @colors[x].to_s }.join.to_i
+  end
+
+end
+
+ResistorColorDuo.value(%w[brown black])
